d69a8eaa612c2b5a40e9838c70a8c7b64f3252f2
VoTaiTri <vo.tai.tri@framgia.com>
2015-02-10 13:55:58 +0700

    Add login/logout for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
+      remember @user
       flash[:success] = "Welcome to the E-Learning"
       redirect_to user_url(@user)
     else
