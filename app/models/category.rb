ae600e2bcd3afac4205569095d9ef8d0efdf8314
Nguyen Hong Viet <nguyen.hong.viet@framgia.com>
2015-02-10 11:24:33 +0700

    show categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :word, dependent: :destroy
-  default_scope -> {order(created_at: :desc)}
-  validate :title, presence: true, length: {maximum: 256}
-  validate :body, presence: true
+  has_many :lesson, dependent: :destroy
+  validates :title, presence: true, length: {maximum: 256}
+  validates :body, presence: true
 end
--- db/migrate/20150209025615_create_words.rb
+++ db/migrate/20150209025615_create_words.rb
@@ -1,12 +1,11 @@
 class CreateWords < ActiveRecord::Migration
   def change
     create_table :words do |t|
-      t.string :vietnamese
       t.string :japanese
       t.references :category, index: true
 
       t.timestamps null: false
     end
-    add_index :words, :vietnamese, unique: true
+    add_index :words, :japanese, unique: true
   end
 end
